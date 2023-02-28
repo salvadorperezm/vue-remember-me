746db99c87a9fe4f5ada8edde3eeea919665414b
salvadorperezm <salvadorperezm115@gmail.com>
2023-02-28 09:41:38 -0600

    page selector added

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -1,14 +1,21 @@
 <template>
     <the-header></the-header>
+    <page-selector @selected-component="componentToDisplay"></page-selector>
 </template>
 
 <script>
+import PageSelector from "./PageSelector.vue";
 import TheHeader from "./TheHeader.vue"
-
 
 export default {
     components: {
+        'page-selector': PageSelector,
         'the-header': TheHeader
+    },
+    methods: {
+        componentToDisplay(component) {
+            console.log('component to display: ', component)
+        }
     }
 }
 </script>
---
+++ src/components/PageSelector.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="page-selector__container">
+        <button class="page-selector__button" @click="selectComponent('stored-resources')">stored resources</button>
+        <button class="page-selector__button" @click="selectComponent('add-resource')">add resource</button>
+    </section>
+</template>
+
+<script>
+export default {
+    created() {
+        this.$emit('selected-component', this.currentComponent)
+    },
+    emits: ['selected-component'],
+    data() {
+        return {
+            currentComponent: 'stored-resources'
+        }
+    },
+    methods: {
+        selectComponent(component) {
+            this.currentComponent = component
+            this.$emit('selected-component', this.currentComponent)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.page-selector__button:first-child {
+    margin-inline-end: 10px;
+}
+
+.page-selector__button {
+    text-transform: capitalize;
+    border: none;
+    padding: 20px;
+    color: var(--purple);
+    background-color: var(--white);
+
+}
+
+.page-selector__button:hover {
+    color: var(--white);
+    background-color: var(--purple);
+    cursor: pointer;
+}
+</style>
