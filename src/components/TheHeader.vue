f44ae25b27179e5f9a701ce45d0e07449d608ede
salvadorperezm <salvadorperezm115@gmail.com>
2023-02-28 08:30:22 -0600

    adding header section

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="header__container">
+        <h1 class="header__text">Remember Me</h1>
+    </header>
+</template>
+
+<style scoped>
+.header__container {
+    background-color: var(--wine);
+    padding-block: 40px;
+    display: flex;
+    justify-content: center;
+}
+
+.header__text {
+    color: var(--white);
+    font-size: 48px;
+}
+</style>
