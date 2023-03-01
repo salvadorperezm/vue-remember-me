86ad1fda6866f85d475f2221af7b6e8a3f50713f
salvadorperezm <salvadorperezm115@gmail.com>
2023-03-01 08:53:42 -0600

    basic component added

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -1,20 +1,35 @@
 <template>
     <the-header></the-header>
     <page-selector @selected-component="componentToDisplay"></page-selector>
+    <component :is="currentComponent" :resources="resources"></component>
 </template>
 
 <script>
 import PageSelector from "./PageSelector.vue";
+import StoredResources from "./StoredResources.vue";
 import TheHeader from "./TheHeader.vue"
 
 export default {
     components: {
         'page-selector': PageSelector,
+        'stored-resources': StoredResources,
         'the-header': TheHeader
+    },
+    data() {
+        return {
+            currentComponent: '',
+            resources: [
+                {
+                    title: 'Google',
+                    description: 'You need to know how to google',
+                    link: 'https://google.com'
+                }
+            ]
+        }
     },
     methods: {
         componentToDisplay(component) {
-            console.log('component to display: ', component)
+            this.currentComponent = component
         }
     }
 }
---
+++ src/components/StoredResources.vue
@@ -0,0 +1,59 @@
+<template>
+    <section v-if="resources" v-for="resource in resources" :key="resource.title" class="resource-item">
+        <div class="resource-item__header">
+            <h2 class="resource-item__title">{{ resource.title }}</h2>
+            <button class="resource-item__button">Delete</button>
+        </div>
+        <div class="resource-item__body">
+            <p class="resource-item__description">{{ resource.description }}</p>
+            <a :href="resource.link" target="_blank" class="resource-item__link">View resource</a>
+        </div>
+    </section>
+    <section v-else>
+        <h2>No resources yet.</h2>
+        <p>Try adding some on the Add Resource page</p>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['resources']
+}
+</script>
+
+<style scoped>
+.resource-item__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-block-end: 10px;
+}
+
+.resource-item__button {
+    border: none;
+    background-color: var(--white);
+    cursor: pointer;
+    color: var(--wine);
+    font-size: 16px;
+    padding: 10px;
+}
+
+.resource-item__button:hover {
+    color: var(--white);
+    background-color: var(--wine);
+    border-radius: 5px;
+}
+
+.resource-item__link {
+    text-decoration: none;
+    color: var(--orange);
+}
+
+.resource-item__description {
+    margin-block-end: 10px;
+}
+
+.resource-item__link:hover {
+    text-decoration: underline;
+}
+</style>
