4ff6949a424433dd4dbd3796491e3a0fac68d274
salvadorperezm <salvadorperezm115@gmail.com>
2023-03-01 10:13:35 -0600

    created form to add resources

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -1,16 +1,18 @@
 <template>
     <the-header></the-header>
     <page-selector @selected-component="componentToDisplay"></page-selector>
-    <component :is="currentComponent" :resources="resources" @return-id="returnId"></component>
+    <component :is="currentComponent" :resources="resources" @return-id="returnId" @new-resource="addResource"></component>
 </template>
 
 <script>
+import AddResource from "./AddResource.vue";
 import PageSelector from "./PageSelector.vue";
 import StoredResources from "./StoredResources.vue";
 import TheHeader from "./TheHeader.vue"
 
 export default {
     components: {
+        'add-resource': AddResource,
         'page-selector': PageSelector,
         'stored-resources': StoredResources,
         'the-header': TheHeader
@@ -27,6 +29,9 @@ export default {
         },
         returnId(id) {
             console.log(id)
+        },
+        addResource(resource) {
+            this.resources.push(resource)
         }
     }
 }
